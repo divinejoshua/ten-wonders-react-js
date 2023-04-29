350e96a9de7c784b6c8e96ef9c8a13450ba3e49e
divinejoshua <divineuche399@gmail.com>
2023-04-29 16:33:20 +0100

    Styling images

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
               type="text" 
               required
               autoFocus={true}
-              placeholder='Ask Didi anything...'
+              placeholder='Search Ten Wonders...'
               className='w-full text-gray-700 text-sm p-2 pt-3 pb-3 pr-5 pl-5 rounded-lg bg-gray-50 focus:outline-none focus:border-blue-500 focus:ring-blue-500 focus:ring-0.5 focus:border-100 transition duration-0 hover:duration-150'
               />
 
@@ -36,7 +36,42 @@ function App() {
               <br></br>
 
             </form>
-        </main>
+
+          {/* Gallery section  */}
+          <section className="gallery mt-10">
+            <div className="image mb-7">
+              <img src="https://images.unsplash.com/photo-1604871000636-074fa5117945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="image" className='rounded-lg shadow' />
+              <div className="caption mt-2 text-gray-500 ml-1">newsite.com</div>
+            </div>
+            <div className="image mb-7">
+              <img src="https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="image" className='rounded-lg'/>
+            </div>
+            <div className="image mb-7">
+              <img src="https://images.unsplash.com/photo-1505178041309-ad46d2e4207b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="image"className='rounded-lg' />
+            </div>
+            <div className="image mb-7">
+              <img src="https://images.unsplash.com/photo-1497114046243-1154db4f4abf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=948&q=80" alt="image" className='rounded-lg' />
+            </div>
+            <div className="image mb-7">
+              <img src="https://images.unsplash.com/photo-1573655349936-de6bed86f839?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGFic3RyYWN0JTIwJTIweWVsbG93fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="image" className='rounded-lg'/>
+            </div>
+            <div className="image mb-7">
+              <img src="https://images.unsplash.com/photo-1541356665065-22676f35dd40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80" alt="image" className='rounded-lg' />
+            </div>
+            <div className="image mb-7">
+              <img src="https://images.unsplash.com/photo-1509114397022-ed747cca3f65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YWJzdHJhY3QlMjAlMjBza3l8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="image" className='rounded-lg'/>
+            </div>
+            <div className="image mb-7">
+              <img src="https://images.unsplash.com/photo-1460411794035-42aac080490a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGFic3RyYWN0JTIwJTIwc2t5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="image" className='rounded-lg' />
+            </div>
+            <div className="image mb-7">
+              <img src="https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGFic3RyYWN0JTIwJTIwc2t5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="image" className='rounded-lg' />
+            </div>
+          </section>
+
+
+
+        </main> 
 
 
   </div>
